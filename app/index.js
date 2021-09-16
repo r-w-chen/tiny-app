8616432f884706f0f63f5e54e869c0b2953c7239
Rebecca Chen <rebeccawchen@gmail.com>
2021-09-16 13:45:47 -0500

    layout items, added event listeners

---
+++ app/index.js
@@ -0,0 +1,26 @@
+console.log('hello world')
+document.addEventListener('DOMContentLoaded', () => {
+const navItems = document.querySelectorAll('.navbar li');
+let currentNavItemIdx = 0;
+/*
+I need to keep track of the current navitem i've clicked
+
+*/
+let header = document.querySelector('.title h1');
+let rightButton = document.querySelector('.right-arrow');
+let leftButton = document.querySelector('.left-arrow');
+rightButton.addEventListener('click', e => {
+    currentNavItemIdx++;
+    console.log('clicked!', currentNavItemIdx)
+    header.innerText = `tiny ${navItems[currentNavItemIdx].innerText}`
+    
+})
+
+leftButton.addEventListener('click', e => {
+    currentNavItemIdx--;
+    console.log('clicked!', currentNavItemIdx)
+})
+})
+
+
+
